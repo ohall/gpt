39a15322264ea1d2c4bbd2c444c9565ced094adf
ohall <ohall@grassroots-tech.com>
2022-12-10 17:04:49 -0500

    require node 18

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const { resolve } = require("path");
 const argArray =  process.argv.slice(2)
 const pipedArg = !process.stdin.isTTY;
 
+if (parseFloat(process.versions.node) < 18 ){
+  console.log('Node 18+ required.');
+  process.exit(1);
+}
+
 if(!pipedArg && !argArray.length){
   console.log('Prompt required.');
   process.exit(1);
