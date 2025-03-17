0be0755b8aac46ec86b8fd571e28b9487ccbe24a
ohall <ohall@grassroots-tech.com>
2025-03-16 20:26:32 -0400

    mod

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 #!/usr/bin/env node
-const { spawnSync } = require("child_process");
-const { resolve } = require("path");
-const argArray =  process.argv.slice(2)
+import { spawnSync } from "child_process";
+import { resolve, dirname } from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const argArray = process.argv.slice(2);
 const pipedArg = !process.stdin.isTTY;
 
-if (parseFloat(process.versions.node) < 18 ){
-  console.log('Node 18+ required.');
+if (parseFloat(process.versions.node) < 20) {
+  console.log('Node 20+ required.');
   process.exit(1);
 }
 
-if(!pipedArg && !argArray.length){
+if (!pipedArg && !argArray.length) {
   console.log('Prompt required.');
   process.exit(1);
 }
 
-const cmd = `${pipedArg?'xargs':''} node --no-warnings ${resolve(__dirname, 'gpt.js')} ${argArray.join(' ')}`;
+const cmd = `${pipedArg ? 'xargs' : ''} node --no-warnings ${resolve(__dirname, 'gpt.js')} ${argArray.join(' ')}`;
 spawnSync(cmd, { stdio: 'inherit', shell: true });
